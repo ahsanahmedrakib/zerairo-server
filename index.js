a03e6f877789eb26b49dab3788f886d477c67644
ahsan-ahmed-rakib <ahsan.raib@technonext.com>
2025-05-22 23:47:38 +0600

    Change DB

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xdeet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@zerairo.uuj4ajh.mongodb.net/zerairo?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
